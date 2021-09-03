8ddcd0921dbd8cecc10214c605625574230660c2
Barry Chen <fm334142@gmail.com>
2021-09-03 16:38:14 +0800

    Make sure `RemoteFeedLoader` delivers success with no items on 200 HTTP response with empty JSON list

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,15 +21,47 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
+			case let .success((data, response)):
 				if response.statusCode != 200 {
 					completion(.failure(Error.invalidData))
 				} else {
-					completion(.failure(Error.invalidData))
+					let decoder = JSONDecoder()
+					do {
+						let root = try decoder.decode(Root.self, from: data)
+						completion(.success(root.feedImages))
+					} catch {
+						completion(.failure(Error.invalidData))
+					}
 				}
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
 		}
+	}
+}
+
+private struct Root: Decodable {
+	let items: [Item]
+
+	var feedImages: [FeedImage] {
+		items.map { $0.feedImage }
+	}
+}
+
+private struct Item: Decodable {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let url: URL
+
+	private enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
+	}
+
+	var feedImage: FeedImage {
+		FeedImage(id: id, description: description, location: location, url: url)
 	}
 }
